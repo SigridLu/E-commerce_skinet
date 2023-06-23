6e9001994ff4f1faa1587de7a23ce76a7d422c57
Yun Lu <ylu6@clemson.edu>
2023-06-23 13:48:47 -0400

    Add nav-bar UI with a simple Http request to get and display all products demo

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Skinet';
+  products: Product[] = [];
+
+  // For DI
+  constructor (private http: HttpClient) { }
+
+  // Make Http request
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response: any) => this.products = response.data, // what to do next
+      error: error => console.log(error), // what to do if there's an error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+      }
+    });
+  }
 }
