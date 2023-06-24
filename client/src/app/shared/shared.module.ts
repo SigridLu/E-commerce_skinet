e4d8b154f3b27df52332ef59860e179f0fc062f9
Yun Lu <ylu6@clemson.edu>
2023-06-23 21:22:19 -0400

    Extract pagination feature out as a reusable component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './paging-header/paging-header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagingHeaderComponent
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot()
   ],
   exports: [
-    PaginationModule
+    PaginationModule,
+    PagingHeaderComponent
   ]
 })
 export class SharedModule { }
